0d99dc10de5218a90521533e9f6c6c4f9d6995f7
unknown <kubap1402@gmail.com>
2025-01-06 18:30:58 +0100

    2022_06_01 mobilna

---
+++ 2022_06_01/012345/mobilna/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Turystyka
+{
+    public partial class MainPage : ContentPage
+    {
+        private int likesNumber = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Like_Button_Clicked(object sender, EventArgs e)
+        {
+            likesNumber++;
+            Set_Likes_Label_Text();
+        }
+
+        private void Remove_Button_Clicked(object sender, EventArgs s)
+        {
+            likesNumber = Math.Max(likesNumber-1, 0);
+            Set_Likes_Label_Text();
+        }
+
+        private void Set_Likes_Label_Text()
+        {
+            likesLabel.Text = String.Format("{0} polubień", likesNumber);
+        }
+    }
+}
