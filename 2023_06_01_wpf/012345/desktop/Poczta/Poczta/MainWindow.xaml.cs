97528bf3ca298a64b988c4332ce984d1466e0d2c
unknown <kubap1402@gmail.com>
2025-01-09 23:24:13 +0100

    '2023_06_01_wpf

---
+++ 2023_06_01_wpf/012345/desktop/Poczta/Poczta/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Poczta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(pocztowka.IsChecked == true)
+            {
+                obraz.Source = new BitmapImage(new Uri("./pocztowka.png", UriKind.Relative));
+                cena.Content = "Cena: 1 zł";
+            }
+            else if(list.IsChecked == true)
+            {
+                obraz.Source = new BitmapImage(new Uri("./list.png", UriKind.Relative));
+                cena.Content = "Cena: 1,5 zł";
+            }
+            else if (paczka.IsChecked == true)
+            {
+                obraz.Source = new BitmapImage(new Uri("./paczka.png", UriKind.Relative));
+                cena.Content = "Cena: 10 zł";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int kod;
+            if (!int.TryParse(kodPocztowy.Text, out kod))
+            {
+                MessageBox.Show("Kod pocztowy powinien się składać z samych cyfr");
+            }
+            else if (kodPocztowy.Text.Length == 5)
+            {
+                MessageBox.Show("Dane przesyłki zostały wprowadzone");
+            }
+            else
+            {
+                MessageBox.Show("Kod pocztowy jest niepoprawny");
+            }
+        }
+    }
+}
---
+++ 2023_06_01_wpf/012345/konsola/Sito/Sito/Program.cs
@@ -0,0 +1,46 @@
+﻿bool[] A = new bool[101];
+WypelnijSito(A);
+List<int> liczbyPierwsze = ZnajdzLiczbyPierwsze(A);
+Console.Write("Liczby pierwsze: ");
+for(int i = 0; i<liczbyPierwsze.Count; i++)
+{
+    Console.Write(liczbyPierwsze[i]);
+    if(i != liczbyPierwsze.Count - 1)
+        Console.Write(", ");
+}
+
+/*
+*******************************************************
+ nazwa funkcji: WypelnijSito
+ parametry wejściowe: tablica (bool []) - tablica wypełniana wartościami
+ wartość zwracana: brak (void)
+ informacje: funkcja pozostawia sito z wartościami true dla liczby pierwszych
+ autor: 012345
+****************************************************
+*/
+void WypelnijSito(bool[] tablica)
+{
+    for(int i = 2; i< tablica.Length; i++)
+        tablica[i] = true;
+
+    for (int i = 2; i * i < tablica.Length; i++)
+    {
+        if (tablica[i] == true)
+        {
+            for (int j = 2 * i; j < tablica.Length; j += i)
+            {
+                tablica[j] = false;
+            }
+        }
+    }
+}
+List<int> ZnajdzLiczbyPierwsze(bool[] tablica)
+{
+    List<int> liczbyPierwsze = [];
+    for (int i = 2; i < tablica.Length; i++)
+    {
+        if (tablica[i])
+            liczbyPierwsze.Add(i);
+    }
+    return liczbyPierwsze;
+}
