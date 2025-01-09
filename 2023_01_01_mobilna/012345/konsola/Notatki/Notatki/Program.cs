06cdf59be3f88380d876da4b31cf4d6887d7798b
unknown <kubap1402@gmail.com>
2025-01-09 21:25:48 +0100

    '2023_01_01_mobilna'

---
+++ 2023_01_01_mobilna/012345/konsola/Notatki/Notatki/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+Notatka notatka1 = new Notatka("Notatka 1", "Tresc notatki 1");
+notatka1.Wyswietl();
+notatka1.WyswietlDaneDiagnostyczne();
+
+Console.WriteLine();
+
+Notatka notatka2 = new Notatka("Notatka 2", "Tresc notatki 2");
+notatka2.Wyswietl();
+notatka2.WyswietlDaneDiagnostyczne();
+
+/*
+************************************************
+ klasa: Notatka
+ opis:  Zawiera informacje o notatce
+ pola:  licznik_notatek (static int) - przechowuje ilość notatek
+        uid (int) - przechowuje unikalny identyfikator notatki
+        tytul (string) - przechowuje tytuł notatki
+        tresc (string) - przechowuje treść notatki
+ autor: 012345
+************************************************
+*/
+class Notatka
+{
+    private static int licznik_notatek = 0;
+    private int uid;
+    protected string tytul, tresc;
+
+    public Notatka(string tytul, string tresc)
+    {
+        licznik_notatek++;
+        this.uid = licznik_notatek;
+        this.tytul = tytul;
+        this.tresc = tresc;
+    }
+
+    public void Wyswietl()
+    {
+        Console.WriteLine("Tytul: {0}\nTresc: {1}", tytul, tresc);
+    }
+
+    public void WyswietlDaneDiagnostyczne()
+    {
+        Console.WriteLine("{0};{1};{2};{3}", licznik_notatek, uid, tytul, tresc);
+    }
+}
---
+++ 2023_01_01_mobilna/012345/mobilna/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notatki
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Notatki { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Notatki = new ObservableCollection<string> { "Zakupy: chleb, masło, ser", "Do zrobienia: obiad, umyć podłogi", "weekend: kino, spacer z psem"};
+            
+            BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Notatki.Add(nowaNotatka.Text);
+        }
+    }
+}
