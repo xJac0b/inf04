a46e5d57946c2bf1f8f4601917b8b88126d60dbf
unknown <kubap1402@gmail.com>
2025-01-09 16:15:53 +0100

    2023_01_01_wpf

---
+++ 2023_01_01_wpf/012345/desktopowa/Pracownik/Pracownik/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pracownik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string password = String.Empty;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int charNumber = int.Parse(charNumberBox.Text);
+            password = String.Empty;
+            string specialCharsSet = "!@#$%^&*()_+-=";
+            string lettersSet = "abcdefghijklmnopqrstuwxyz";
+            string upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+            if ((digits.IsChecked ?? false) && password.Length < charNumber)
+            {
+                password += new Random().Next(0, 10);
+            }
+            if ((specialChars.IsChecked ?? false) && password.Length < charNumber)
+            {
+                password += specialCharsSet[new Random().Next(0, specialCharsSet.Length)];
+            }
+            if ((differentCases.IsChecked ?? false) && password.Length < charNumber)
+            {
+                password += upperCaseSet[new Random().Next(0, upperCaseSet.Length)];
+            }
+
+            while(password.Length < charNumber)
+            {
+                password += lettersSet[new Random().Next(0, lettersSet.Length)];
+            }
+
+            MessageBox.Show(password);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string firstName = firstNameBox.Text;
+            string lastName = lastNameBox.Text;
+            string role = roleComboBox.Text;
+            MessageBox.Show(String.Format("Dane pracownika: {0} {1} {2} Hasło: {3}", firstName,
+                lastName, role, (password ?? "")));
+
+        }
+    }
+}
---
+++ 2023_01_01_wpf/012345/konsola/Nwd/Nwd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+Console.WriteLine("Podaj 1. liczbę:");
+int a = int.Parse(Console.ReadLine());
+if (a <= 0)
+{
+    Console.WriteLine("Liczba musi być całkowita dodatnia");
+    return;
+}
+Console.WriteLine("Podaj 2 liczbę:");
+int b = int.Parse(Console.ReadLine());
+if (b <= 0)
+{
+    Console.WriteLine("Liczba musi być całkowita dodatnia");
+    return;
+}
+
+int nwd = Nwd(a, b);
+Console.WriteLine("Nwd wynosi {0}", nwd);
+
+
+//**********************************************
+// nazwa funkcji: Nwd
+// opis funkcji: liczy najwyższy wspólny dzielnik dwóch podanych liczb
+// parametry: a - pierwszy czynnik
+// b - drugi czynnik
+// zwracany typ i opis: (int) najwyższy wspólny dzielnik
+// autor: 012345
+//***********************************************
+int Nwd(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b) a -= b;
+        else b -= a;
+    }
+    return a;
+}
+
