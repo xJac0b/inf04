3fe283cccc868a58084b9cbae122fd0e47518f9a
unknown <kubap1402@gmail.com>
2025-01-10 13:31:24 +0100

    2023_06_02_mobilka

---
+++ 2023_06_02_mobilka/012345/konsola/Sortowanie/Sortowanie/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] tablica = new int[100];
+for(int i = 0; i < tablica.Length; i++)
+{
+    tablica[i] = new Random().Next(0, 1001);
+}
+Console.WriteLine("Tablica przed posortowaniem: ");
+foreach(int i in tablica)
+{
+    Console.Write(i + ", ");
+}
+Sortuj(tablica);
+Console.WriteLine();
+Console.WriteLine("Tablica po posortowaniu: ");
+foreach (int i in tablica)
+{
+    Console.Write(i + ", ");
+}
+
+void Sortuj(int[] tablica)
+{
+    bool zamiana = true;
+    while(zamiana)
+    {
+        zamiana = false;
+        for(int i = 0; i< tablica.Length-1; i++)
+        {
+            if (tablica[i] > tablica[i+1])
+            {
+                int temp = tablica[i];
+                tablica[i] = tablica[i + 1];
+                tablica[i + 1] = temp;
+                zamiana = true;
+            }
+        }
+    }
+}
---
+++ 2023_06_02_mobilka/012345/mobilna/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Czcionka
+{
+    public partial class MainPage : ContentPage
+    {
+        int indeks = 0;
+        string[] cytaty = new string[3] { "Dzień dobry", "Good morning", "Buenos dias" };
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            slider.Value = Math.Round(slider.Value);
+            rozmiar.Text = String.Format("Rozmiar: {0}", slider.Value);
+            cytat.FontSize = slider.Value;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            indeks++;
+            if (indeks >= cytaty.Length)
+                indeks = 0;
+            cytat.Text = cytaty[indeks];
+        }
+    }
+}
