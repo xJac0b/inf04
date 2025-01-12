e0716b27048b724f93e02c21eb07d79d8eaa1aa8
unknown <kubap1402@gmail.com>
2025-01-12 16:54:26 +0100

    2024_01_01_wpf

---
+++ 2024_01_01_wpf/012345/desktopowa/Paszport/Paszport/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Paszport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string imie = poleImie.Text;
+            string nazwisko = poleNazwisko.Text;
+            if (imie == String.Empty || nazwisko == String.Empty)
+            {
+                MessageBox.Show("Wprowadź dane");
+                return;
+            }
+            string kolorOczu = String.Empty;
+            if (niebieskie.IsChecked == true)
+            {
+                kolorOczu = "niebieskie";
+            }
+            else if (zielone.IsChecked == true)
+            {
+                kolorOczu = "zielone";
+            }
+            else
+            {
+                kolorOczu = "piwne";
+            }
+            MessageBox.Show(String.Format("{0} {1} kolor oczu {2}", imie, nazwisko, kolorOczu));
+
+        }
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string sciezkaDoZdjecia = String.Format("{0}-zdjecie.jpg", poleNumer.Text);
+            Trace.WriteLine(sciezkaDoZdjecia);
+            string sciezkaDoOdcisku = String.Format("{0}-odcisk.jpg", poleNumer.Text);
+            zdjecie.Source = new BitmapImage(new Uri(sciezkaDoZdjecia, UriKind.Relative));
+            odcisk.Source = new BitmapImage(new Uri(sciezkaDoOdcisku, UriKind.Relative));
+        }
+    }
+}
---
+++ 2024_01_01_wpf/012345/konsola/Pesel/Pesel/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection.Metadata;
+using System;
+
+string peselString = Console.ReadLine();
+int[] pesel = new int[11];
+for (int i = 0; i<peselString.Length; i++)
+{
+    pesel[i] = peselString[i]-'0';
+}
+
+char plec = SprawdzPlec(pesel);
+if (plec == 'K')
+{
+    Console.WriteLine("Kobieta");
+}
+else
+    Console.WriteLine("Mężczyzna");
+
+bool zgodnoscSumyKontrolnej = SprawdzPesel(pesel);
+if (zgodnoscSumyKontrolnej)
+    Console.WriteLine("Suma kontrolna zgodna z podanym numerem PESEL");
+else
+    Console.WriteLine("Suma kontrolna niezgodna z podanym numerem PESEL");
+
+
+char SprawdzPlec(int[] pesel)
+{
+    if (pesel[9] % 2 == 0)
+        return 'K';
+    return 'M';
+}
+
+/*
+**********************************************
+nazwa funkcji: SprawdzPesel
+opis funkcji: Funkcja sprawdza zgodność sumy kontrolnej (ostatnia cyfra) z resztą numeru PESEL
+parametry: pesel (int []) - przechowuje numer PESEL
+zwracany typ i opis: bool - zwraca true gdy suma kontrolna jest zgodna z resztą numeru PESEL
+autor: 012345
+***********************************************
+*/
+bool SprawdzPesel(int[] pesel)
+{
+    int[] wagi = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+    int suma = 0;
+    int suma_kontrolna = 0;
+    for(int i = 0; i < wagi.Length; i++)
+        suma += (pesel[i] * wagi[i]);
+    int modulo = suma % 10;
+    if (modulo != 0)
+        suma_kontrolna = 10 - modulo;
+    Console.WriteLine(suma_kontrolna);
+    return pesel[10] == suma_kontrolna;
+}
